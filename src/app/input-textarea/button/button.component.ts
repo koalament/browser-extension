90cafea41744cdc52283bc0ecd30ba92b571489b
koalament <koalament@gmail.com>
2020-04-17 14:08:09 +0430

    button calculator

--- src/app/input-textarea/button/button.component.ts
+++ src/app/input-textarea/button/button.component.ts
@@ -8,12 +8,17 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ButtonComponent implements OnInit {
 
   @Input() label: string = 'POST';
-  @Input('stringCount') stringCount: number = 0;
+  @Input() stringLength: number = 0;
   priceString: string = "$0.0"
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges() {
+    let charge = Math.floor(this.stringLength) * 0.01;
+    this.priceString = `$${charge.toFixed(2)}`
   }
 
 }
