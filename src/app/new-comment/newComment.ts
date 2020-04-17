5c937026f8fc15a773c4d432e1a0ec6e99dc9ede
koalament <koalament@gmail.com>
2020-04-17 18:13:47 +0430

    update

--- src/app/new-comment/newComment.ts
+++ src/app/new-comment/newComment.ts
@@ -1,8 +1,13 @@
 export class NewComment {
+    public content: string | null = null;
+
     constructor(public name: string) { }
+
+    setComment(comment: string) {
+        this.content = comment;
+    }
 
     test() {
         console.log(this.name);
-
     }
 }
