5346a233769b507a97156556868f510e850d7d62
koalament <koalament@gmail.com>
2020-04-17 18:13:18 +0430

    comment confirmation working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,19 @@ import { NewComment } from './new-comment/newComment';
 })
 export class AppComponent {
   title: string = 'koalament';
-  // newComment: NewComment | null = null;
-  newComment: NewComment | null = new NewComment('milad');
+  newComment: NewComment | null = null;
+  state: 'name' | 'comment' | 'pay' = 'name';
+  // newComment: NewComment | null = new NewComment('milad');
 
   onNameSubmit(name: string) {
     this.newComment = new NewComment(name);
     this.newComment.test();
+    this.state = 'comment';
+  }
+
+  onCommentSubmit(comment: string) {
+    this.newComment.setComment(comment);
+    // console.log(this.newComment);
+    this.state = 'pay';
   }
 }
