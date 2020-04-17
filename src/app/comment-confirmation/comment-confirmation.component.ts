fabfa57918d8a9b9d79895dbe1f9d1dbba30159f
koalament <koalament@gmail.com>
2020-04-17 17:07:46 +0430

    comment confirmation added

---
+++ src/app/comment-confirmation/comment-confirmation.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import * as moneyButton from "./money-button.js";
+
+@Component({
+  selector: 'app-comment-confirmation',
+  templateUrl: './comment-confirmation.component.html',
+  styleUrls: ['./comment-confirmation.component.scss']
+})
+export class CommentConfirmationComponent implements OnInit {
+
+  @ViewChild('moneyBtnHere') moneyBtnHere;
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    moneyButton.render(this.moneyBtnHere.nativeElement, {
+      label: "Send",
+      clientIdentifier: "36a0fd92080022d9234e610de329e13d",
+      buttonId: "234325",
+      outputs: [
+        {
+          to: '14781',
+          amount: '0.005',
+          currency: 'USD'
+        },
+        {
+          script: 'OP_FALSE OP_RETURN ' + 'test',
+          amount: '0',
+          currency: 'USD'
+        }
+      ],
+      onPayment: function (arg) {
+        console.log(arg);
+      },
+      onError: function (arg) { 
+        // alert("Something went wrong!"); console.log('onError', arg) 
+      }
+    })
+  }
+
+  ngOnInit() {
+  }
+
+}
