03c89908131705efc38086ba925cca771a1e657b
koalament <koalament@gmail.com>
2020-04-19 19:00:30 +0430

    environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  SOCKET_ENDPOINT: 'https://dev-nap.koalament.io',
+  LAYER_VERSION: 1
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  SOCKET_ENDPOINT: 'https://dev-nap.koalament.io',
+  LAYER_VERSION: 1
 };
 
 /*
