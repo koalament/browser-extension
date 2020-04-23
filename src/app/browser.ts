e36a1c04c7aa7619f958be4d095fec1c254b0d97
koalament <koalament@gmail.com>
2020-04-24 04:18:14 +0430

    browser condition

--- src/app/browser.ts
+++ src/app/browser.ts
@@ -3,12 +3,16 @@ import { environment } from 'src/environments/environment';
 export function firefox(callback) {
     if (environment.browser.name === "firefox") {
         callback();
+        return true;
     }
+    return false;
 }
 
 
 export function chrome(callback) {
     if (environment.browser.name === "chrome") {
         callback();
+        return true;
     }
+    return false;
 }
