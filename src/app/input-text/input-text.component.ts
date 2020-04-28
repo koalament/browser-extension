e7b2f50ced9018061b07700a58b7dc461a173b51
koalament <koalament@gmail.com>
2020-04-28 21:43:54 +0430

    input name new design

--- src/app/input-text/input-text.component.ts
+++ src/app/input-text/input-text.component.ts
@@ -9,6 +9,7 @@ export class InputTextComponent implements OnInit {
   @Input('placeholder') placeholder: string = "";
   @Input('submitValue') submitValue: string = "submit";
   @Output('onSubmit') onSubmit: EventEmitter<string> = new EventEmitter<string>();
+  @Output() onClose: EventEmitter<void> = new EventEmitter();
   @Input() value: string = "";
 
   constructor() { }
@@ -21,4 +22,12 @@ export class InputTextComponent implements OnInit {
     e.preventDefault();
   }
 
+  close(e) {
+    this.onClose.emit();
+  }
+
+  prevent(e: Event) {
+    e.stopPropagation();
+    return;
+  }
 }
